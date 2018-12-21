756ea3fef17c88322fa9f40e5ab34bf9db9f61ae
HectorCorro <hecorroza88@gmail.com>
2018-12-21 03:07:33 -0600

    app

---
+++ app/aplication.py
@@ -0,0 +1,21 @@
+from flask import Flask, url_for, jsonify, request, json
+import pickle
+import numpy as np
+
+with open("model3.pkl", "rb") as f:
+	model = pickle.load(f)
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST'])
+
+def predict():
+	data = request.get_json(force=True)
+	results = {}
+	y_hat = model.predict(np.array(data))
+	results['parameters']= data
+	results['resultado'] = y_hat.tolist()
+	return jsonify(results)
+
+if __name__ == '__main__':
+	app.run(debug=True, port=5000)
