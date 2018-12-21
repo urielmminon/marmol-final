0048b12df974fb3eeb45077ce69f67974299b2a3
urielmminon <urielmiranda@MacBook-Air-de-Uriel.local>
2018-12-21 01:06:22 -0600

    now

--- auto.sh
+++ auto.sh
@@ -3,9 +3,8 @@
 mkdir Datos
 cd Datos
 
-curl -L -o test.csv.zip "https://drive.google.com/uc?export=download&id=1IbIfNdpBJf2R9l_bgWkENes3-FWyWZ86"
-curl -L -o train.csv.zip "https://drive.google.com/uc?export=download&id=1Vrr4TvX0DoPEJzgbCjcUGnHi0hueiGgt"
-
+curl -L -o test.csv.zip "https://drive.google.com/uc?export=download&id=144qWTDn64Lf2-SIIQwxLnwWu3ubIxIPu"
+curl -L -o train.csv.zip "https://drive.google.com/uc?export=download&id=1mV-LmKfbyIi209YVBlGBkZiVqwB4un2e"
 unzip train.csv.zip
 unzip test.csv.zip
 
